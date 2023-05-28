8e13b8cf38e945bb5a18d125f1d1c1028e890141
miles632 <113833646+miles632@users.noreply.github.com>
2023-05-28 19:13:30 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod testing;
+
+use std::env;
+use std::process;
+use std::fs;
+
+use minigrep::Config;
+
+fn main()
+{
+    // stores command line args
+    let _args: Vec<String> = env::args().collect();
+
+    let config1 = Config::parse_config(env::args())
+    .unwrap_or_else(|_err| {
+        eprintln!("Problem parsing arguments {_err}");
+        process::exit(1);
+    });
+
+    let _contents = fs::read_to_string(&config1.file_path)
+        .expect("Should have been able to read the file");
+
+
+    if let Err(e) =  minigrep::run(config1)
+    {
+        eprintln!("Encountered runtime error: {e}");
+        process::exit(1);
+    }
+}
